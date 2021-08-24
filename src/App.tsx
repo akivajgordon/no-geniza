45bd9b15900e2a086ecc2329d81e7df272175061
Akiva Gordon <akivag29@gmail.com>
2021-08-24 00:04:36 -0400

    Add "Copy to clipboard" button

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,74 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import noGeniza from './no-geniza'
 
-const SplitLayout: React.FC<{}> = ({ children }) => <div className="SplitLayout">{children}</div>
+const SplitLayout: React.FC<{}> = ({ children }) => {
+  return <div className="SplitLayout">{children}</div>
+}
+
+const Button: React.FC<{ onClick(): void }> = ({ children, onClick }) => {
+  return (
+    <button className="Button" onClick={onClick}>
+      {children}
+    </button>
+  )
+}
 
 function App() {
   const [input, setInput] = React.useState('')
 
+  const processed = noGeniza(input)
+
   return (
     <div style={{ padding: '1em' }}>
-    <SplitLayout>
-      <div>
-      <label style={{display: 'block'}}>Source</label>
-      <textarea dir="rtl" style={{width: '100%', maxWidth: '100%', boxSizing: 'border-box'}} value={input} onChange={e => setInput(e.target.value)} />
-      </div>
-      <div>
-      <label style={{display: 'block'}}>Processed</label>
-      <textarea dir="rtl" style={{width: '100%', maxWidth: '100%', boxSizing: 'border-box'}} value={noGeniza(input)} disabled />
-
-      </div>
-    </SplitLayout>
+      <SplitLayout>
+        <div style={{ alignSelf: 'flex-end' }}>
+          <label style={{ display: 'block' }}>Source</label>
+          <textarea
+            dir="rtl"
+            style={{
+              width: '100%',
+              maxWidth: '100%',
+              boxSizing: 'border-box',
+            }}
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+        <div style={{ alignSelf: 'flex-end' }}>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'flex-end',
+            }}
+          >
+            <label style={{ display: 'block' }}>Processed</label>
+            <Button
+              onClick={() => {
+                navigator.clipboard.writeText(processed)
+              }}
+            >
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <img src="/content_copy_black_24dp.svg" height="16" />
+                <p style={{ marginLeft: '0.25rem' }}>Copy to clipboard</p>
+              </div>
+            </Button>
+          </div>
+          <textarea
+            dir="rtl"
+            style={{
+              width: '100%',
+              maxWidth: '100%',
+              boxSizing: 'border-box',
+            }}
+            value={processed}
+            disabled
+          />
+        </div>
+      </SplitLayout>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/no-geniza.ts
+++ src/no-geniza.ts
@@ -8,26 +8,45 @@ const SOF_PASUK = '\u05c3'
 const UNICODE_NIKKUD_AND_CANTILLATION_START = '\u0590'
 const UNICODE_NIKKUD_AND_CANTILLATION_END = '\u05cf'
 
-const regexRange = ({ start, end, excluding }: { start: string, end: string, excluding: [string] }): string => {
+const regexRange = ({
+  start,
+  end,
+  excluding,
+}: {
+  start: string
+  end: string
+  excluding: [string]
+}): string => {
   const excluded = excluding[0]
 
-  return `[${start}-${String.fromCodePoint(excluded.codePointAt(0)! - 1)}${String.fromCodePoint(excluded.codePointAt(0)! + 1)}-${end}]`
+  return `[${start}-${String.fromCodePoint(
+    excluded.codePointAt(0)! - 1
+  )}${String.fromCodePoint(excluded.codePointAt(0)! + 1)}-${end}]`
 }
 
-const REGEXP_NIKKUD_AND_CANTILLATION_RANGE_EXCLUDING_SOF_PASUK = regexRange({ start: UNICODE_NIKKUD_AND_CANTILLATION_START, end: UNICODE_NIKKUD_AND_CANTILLATION_END, excluding: [SOF_PASUK]})
+const REGEXP_NIKKUD_AND_CANTILLATION_RANGE_EXCLUDING_SOF_PASUK = regexRange({
+  start: UNICODE_NIKKUD_AND_CANTILLATION_START,
+  end: UNICODE_NIKKUD_AND_CANTILLATION_END,
+  excluding: [SOF_PASUK],
+})
 
 const like = (word: string): RegExp => {
-  return new RegExp(`${word.split('').map(l => `${l}${REGEXP_NIKKUD_AND_CANTILLATION_RANGE_EXCLUDING_SOF_PASUK}*`).join('')}`, 'g')
+  return new RegExp(
+    `${word
+      .split('')
+      .map(
+        (l) =>
+          `${l}${REGEXP_NIKKUD_AND_CANTILLATION_RANGE_EXCLUDING_SOF_PASUK}*`
+      )
+      .join('')}`,
+    'g'
+  )
 }
 
 export default (s: string): string => {
   return s
-    .replace(
-      like([YUD, HEY, VAV, HEY].join('')),
-      'ה׳'
-    )
-    .replace(
-      like([ALEPH, LAMED, HEY, YUD, MEM_SOFIT].join('')),
-      match => match.split(LAMED).join(`-${LAMED}`)
+    .replace(like([YUD, HEY, VAV, HEY].join('')), 'ה׳')
+    .replace(like([ALEPH, LAMED, HEY, YUD, MEM_SOFIT].join('')), (match) =>
+      match.split(LAMED).join(`-${LAMED}`)
     )
 }
